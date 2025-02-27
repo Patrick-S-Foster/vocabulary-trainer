97f21de46a93eb6870c53966bd355c24d963de5b
Patrick Foster <foster.s.patrick@gmail.com>
2025-02-27 21:53:14 +0100

    Initial implementation of the home content.

---
+++ app/src/main/java/com/example/vocabularytrainer/ui/content/HomeContent.kt
@@ -0,0 +1,65 @@
+package com.example.vocabularytrainer.ui.content
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.vocabularytrainer.R
+import com.example.vocabularytrainer.service.db.LanguageLevel
+import com.example.vocabularytrainer.ui.components.LanguageProgressIndicator
+import com.example.vocabularytrainer.ui.components.TitledComposable
+import com.example.vocabularytrainer.ui.components.WordCard
+
+@Composable
+fun HomeContent(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.home_content_spacing))
+    ) {
+        TitledComposable(stringResource(R.string.word_of_the_day)) {
+            WordCard(LanguageLevel.A1, "word", "phonetic", "null")
+        }
+
+        TitledComposable(stringResource(R.string.progress)) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.home_content_indicators_spacing))
+            ) {
+                items(
+                    LanguageLevel.entries.take(LanguageLevel.entries.size - 1).toList()
+                ) { languageLevel ->
+                    LanguageProgressIndicator(
+                        Modifier.size(125.dp),
+                        languageLevel,
+                        10,
+                        100
+                    )
+                }
+                item(span = { GridItemSpan(2) }) {
+                    LanguageProgressIndicator(
+                        Modifier.size(125.dp),
+                        LanguageLevel.entries.last(),
+                        10,
+                        100
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeContentPreview() {
+    HomeContent(Modifier)
+}
